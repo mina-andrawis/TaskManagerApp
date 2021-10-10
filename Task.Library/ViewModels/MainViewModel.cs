46a28f446c75d2493bfa8f264d85be9993098bc9
Mina Andrawis <maa16s@my.fsu.edu>
2021-10-10 14:48:58 -0400

    Added skeleton code for Models, ViewModels, UWP page

---
+++ Task.Library/ViewModels/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task.Library.Models;
+
+namespace Task.Library.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<ItemBase> taskList { get; set; }
+
+        public ItemBase SelectedItem { get; set; }
+
+        public MainViewModel ()
+        {
+            // make sure the taskList is never null since binding can not
+            // bind to null reference
+            taskList = new List<ItemBase>(); 
+        }
+
+        public void AddItem()
+        {
+            if (SelectedItem == null)
+            {
+                taskList.Add(new ItemBase());
+            }
+        }
+
+        public void DeleteItem()
+        {
+            if (SelectedItem != null)
+            {
+                taskList.Remove(SelectedItem);
+            }
+        }
+    }
+}
---
+++ TaskManagerUWP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TaskManagerUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void AddNew_Click(object sender, RoutedEventArgs e)
+        {
+            /*var diag = new TicketDialog((DataContext as MainViewModel).SupportTickets);
+            await diag.ShowAsync();*/
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            /*(DataContext as MainViewModel).Remove();*/
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            /*(DataContext as MainViewModel).Remove();*/
+        }
+    }
+}
