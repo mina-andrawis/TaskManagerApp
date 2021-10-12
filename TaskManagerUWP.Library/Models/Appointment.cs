1a6100d8c783e15251fe78f992aa678c39b1da95
Mina Andrawis <maa16s@my.fsu.edu>
2021-10-11 22:35:19 -0400

    Working Skelly

---
+++ TaskManagerUWP.Library/Models/Appointment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task.Library.Models
+{
+    public class Appointment : ItemBase
+    {
+        public DateTime Start { set; get; }
+        public DateTime Stop { set; get; }
+        public List<String> Attendees { set; get; }
+
+        public Appointment AddAppointment(string name, string desc, DateTime start, DateTime stop, List<String> attendees)
+        {
+
+            Appointment newAppt = new Appointment();
+
+            newAppt.Name = name;
+            newAppt.Description = desc;
+            newAppt.Start = start;
+            newAppt.Stop = stop;
+            newAppt.Attendees = attendees;
+
+            return newAppt;
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{Id}.{Name} - {Description} \n" +
+                $"  Start Time: {Start}\n" +
+                $"  End Time: {Stop}\n" +
+                $"  Attendees: { string.Join(", ", Attendees)}";
+        }
+    }
+}
